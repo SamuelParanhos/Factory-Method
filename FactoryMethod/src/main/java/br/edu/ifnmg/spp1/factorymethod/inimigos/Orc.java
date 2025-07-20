0841de70a1e09c70c06849882adeb08abed81507
Samuel Paranhos <sanparanhos@gmail.com>
2025-07-20 10:33:18 -0300

    Inimigo do tipo Orc

---
+++ FactoryMethod/src/main/java/br/edu/ifnmg/spp1/factorymethod/inimigos/Orc.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifnmg.spp1.factorymethod.inimigos;
+
+/**
+ *
+ * @author rdpp
+ */
+public class Orc 
+        extends StatusInimigos 
+        implements Inimigo {
+
+    public Orc(int nivel, float vida, float defesa, float ataque) {
+        super(nivel, vida, defesa, ataque);
+    }
+
+    @Override
+    public void tipoDeInimigo() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
